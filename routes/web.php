c0f34872f35029b4be7e43495f034f3865a49c6f
soedahideyuki <kiyudehi811@gmail.com>
2024-03-27 19:17:19 +0900

    kadai完了

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function(){
   return view('welcome');
 });
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{id}', [PostController::class, 'show']);
